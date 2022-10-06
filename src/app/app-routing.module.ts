97fe27b648a3aa17f36101af9d8e5a07a8977427
unknown <volpeluigi65@hotmail.it>
2022-10-06 18:26:25 +0200

    go

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    pathMatch:'full',
+    component:HomepageComponent
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
+  declarations:[HomepageComponent,NotFoundComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitlePipe } from './pipes/title.pipe';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlePipe,
   ],
   imports: [
     BrowserModule,
